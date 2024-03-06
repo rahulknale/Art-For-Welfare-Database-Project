6ac4905c13bd3ce520db3317fb02609e4591d0e6
Rahul Nale <154226006+rahulknale@users.noreply.github.com>
2024-03-06 17:21:01 +0530

    all SQL files commit

---
+++ delete_records.sql
@@ -0,0 +1,29 @@
+delete from transactions;
+alter table transactions
+auto_increment=1;
+
+
+
+
+delete from customers;
+alter table customers
+auto_increment=1;
+
+
+
+delete from artworks;
+alter table artworks
+auto_increment=1;
+
+
+
+
+delete from artists;
+alter table artists
+auto_increment=1;
+
+
+
+delete from ngo;
+alter table ngo
+auto_increment=1;
